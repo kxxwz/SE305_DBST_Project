edfaba11206b5acdff88b933b4ea570aaaf7ce8e
CoolPhilChen <chenhongjie@sjtu.edu.cn>
2018-12-04 23:39:43 +0800

    upload code for processing book info

---
+++ code/query.py
@@ -0,0 +1,11 @@
+# Find the books according to the name, author.
+def find_book(book_name, author):
+	retun 'book_url'
+
+# Given a book and chapter number, you can return some relative pictures.
+def find_pic_by_name_chapter(book_name, chapter_num):
+	return ['01.jpg', '02.jpg']
+
+# Given some words, you can find the relative pictures according to the words.
+def find_pic_by_words(book_name, words):
+	return ['01.jpg', '02.jpg']
